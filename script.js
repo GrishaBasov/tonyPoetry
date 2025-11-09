601c1fc6bc06199bbd7ae44006d5851db8bd8283
g.basov <grigoriy.basovvv@gmail.com>
2025-11-09 14:40:54 +0300

    Добавить стихи, фавикон и мета-теги

--- script.js
+++ script.js
@@ -2,9 +2,17 @@ document.addEventListener('DOMContentLoaded', function() {
     const poemElement = document.getElementById('poem');
     const randomPoem = poems[Math.floor(Math.random() * poems.length)];
 
+    const lines = randomPoem.split('\n');
+    while (lines.length && lines[lines.length - 1].trim() === '') {
+        lines.pop();
+    }
 
-    poemElement.innerHTML = randomPoem.split('\n').map(line => line.trim() === ''
-            ? '<div class="empty-line"></div>'
-            : `<span class="poem-line">${line}</span><br>`
-    ).join('');
+    poemElement.innerHTML = lines.map((line, index) => {
+        if (line.trim() === '') {
+            return '<div class="empty-line"></div>';
+        }
+
+        const isLastLine = index === lines.length - 1;
+        return `<span class="poem-line">${line}</span>${isLastLine ? '' : '<br>'}`;
+    }).join('');
 });
